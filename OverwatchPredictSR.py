84ee53709a0cfdfea57f870ac8380b1a517190e1
Shrivu Shankar <shrivu1122@gmail.com>
2017-09-05 13:44:33 -0500

    Update

--- OverwatchPredictSR.py
+++ OverwatchPredictSR.py
@@ -5,19 +5,20 @@
 
 # Imports
 
-from OverwatchProcessData import get_competitive_rank, get_vector_gamestats, get_vector_herostats, get_vector_combined, general_stats, hero_stats
+from OverwatchProcessData import get_competitive_rank, get_vector_gamestats, get_vector_herostats
+from OverwatchProcessData import get_vector_combined, general_stats, hero_stats
 from OverwatchGatherData import Player, find_usernames
+
+import numpy as np
+import os
+
+np.random.seed(3)
 
 from sklearn.preprocessing import StandardScaler
 from keras.layers import Dense, Dropout
 from keras.models import Sequential, load_model
 
 import matplotlib.pyplot as plt
-
-import numpy as np
-import os
-
-np.random.seed(5)
 
 
 # In[2]:
@@ -50,31 +51,7 @@ def generate_players():
         
         yield Player.from_file(os.path.join('profiles', filename))
 
-def load_data():
-
-    unscaled_X, unscaled_y = [], []
-
-    for player in generate_players():
-
-        rank = get_competitive_rank(player, 'us')
-        rank2 = get_competitive_rank(player, 'eu')
-
-        if rank: # Only use data w/rank attached
-
-            unscaled_X.append(get_vector_gamestats(player, 'us'))
-            unscaled_y.append(rank)
-            
-        if rank2:
-
-            unscaled_X.append(get_vector_gamestats(player, 'eu'))
-            unscaled_y.append(rank2)
-
-    unscaled_X = np.array(unscaled_X, dtype=np.float64)
-    unscaled_y = np.array(unscaled_y, dtype=np.float64)
-    
-    return unscaled_X, unscaled_y
-
-def load_data2():
+def load_data(get_vector):
 
     unscaled_X, unscaled_y = [], []
 
@@ -85,88 +62,19 @@ def load_data2():
 
         if rank:
 
-            unscaled_X.append(get_vector_gamestats(player, 'us', stat_keys=general_general_stats))
+            unscaled_X.append(get_vector(player, 'us'))
             unscaled_y.append(rank)
             
         if rank2:
             
-            unscaled_X.append(get_vector_gamestats(player, 'eu', stat_keys=general_general_stats))
+            unscaled_X.append(get_vector(player, 'eu'))
             unscaled_y.append(rank2)
 
     unscaled_X = np.array(unscaled_X, dtype=np.float64)
     unscaled_y = np.array(unscaled_y, dtype=np.float64)
     
-    return unscaled_X, unscaled_y
-
-def load_data3():
-
-    unscaled_X, unscaled_y = [], []
-
-    for player in generate_players():
-
-        rank = get_competitive_rank(player, 'us')
-        rank2 = get_competitive_rank(player, 'eu')
-
-        if rank:
-
-            unscaled_X.append(get_vector_herostats(player, 'us'))
-            unscaled_y.append(rank)
-            
-        if rank2:
-            
-            unscaled_X.append(get_vector_herostats(player, 'eu'))
-            unscaled_y.append(rank2)
-
-    unscaled_X = np.array(unscaled_X, dtype=np.float64)
-    unscaled_y = np.array(unscaled_y, dtype=np.float64)
-    
-    return unscaled_X, unscaled_y
-
-def load_data4():
-
-    unscaled_X, unscaled_y = [], []
-
-    for player in generate_players():
-
-        rank = get_competitive_rank(player, 'us')
-        rank2 = get_competitive_rank(player, 'eu')
-
-        if rank:
-
-            unscaled_X.append(get_vector_herostats(player, 'us', stat_keys=general_hero_stats))
-            unscaled_y.append(rank)
-            
-        if rank2:
-            
-            unscaled_X.append(get_vector_herostats(player, 'eu', stat_keys=general_hero_stats))
-            unscaled_y.append(rank2)
-
-    unscaled_X = np.array(unscaled_X, dtype=np.float64)
-    unscaled_y = np.array(unscaled_y, dtype=np.float64)
-    
-    return unscaled_X, unscaled_y
-
-def load_data5():
-
-    unscaled_X, unscaled_y = [], []
-
-    for player in generate_players():
-
-        rank = get_competitive_rank(player, 'us')
-        rank2 = get_competitive_rank(player, 'eu')
-
-        if rank:
-
-            unscaled_X.append(get_vector_combined(player, 'us'))
-            unscaled_y.append(rank)
-            
-        if rank2:
-            
-            unscaled_X.append(get_vector_combined(player, 'eu'))
-            unscaled_y.append(rank2)
-
-    unscaled_X = np.array(unscaled_X, dtype=np.float64)
-    unscaled_y = np.array(unscaled_y, dtype=np.float64)
+    print(unscaled_X.shape)
+    print(unscaled_y.shape)
     
     return unscaled_X, unscaled_y
 
@@ -269,6 +177,7 @@ def get_model4(from_file=False):
         model.add(Dense(8, kernel_initializer='normal', activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(8, kernel_initializer='normal', activation='relu'))
+        model.add(Dropout(0.5))
         model.add(Dense(1, kernel_initializer='normal'))
 
         model.compile(loss='mean_squared_error', optimizer='adam')
@@ -325,47 +234,11 @@ def predict_sr(model, player):
     
     return int(sr)
 
-def predict_sr2(model, player):
+def predict_sr2(model, player, scaler_for_X, get_vector):
     
-    stats_vector = np.array([get_vector_gamestats(player, 'us', stat_keys=general_general_stats)])
+    stats_vector = np.array([get_vector(player, 'us')])
     
-    X = scaler_X2.transform(stats_vector)
-
-    y_matrix = model.predict(X)
-    
-    sr = np.squeeze(y_matrix) * 5000
-    
-    return int(sr)
-
-def predict_sr3(model, player):
-    
-    stats_vector = np.array([get_vector_herostats(player, 'us')])
-    
-    X = scaler_X3.transform(stats_vector)
-
-    y_matrix = model.predict(X)
-    
-    sr = np.squeeze(y_matrix) * 5000
-    
-    return int(sr)
-
-def predict_sr4(model, player):
-    
-    stats_vector = np.array([get_vector_herostats(player, 'us', stat_keys=general_hero_stats)])
-    
-    X = scaler_X4.transform(stats_vector)
-
-    y_matrix = model.predict(X)
-    
-    sr = np.squeeze(y_matrix) * 5000
-    
-    return int(sr)
-
-def predict_sr5(model, player):
-    
-    stats_vector = np.array([get_vector_combined(player, 'us')])
-    
-    X = scaler_X5.transform(stats_vector)
+    X = scaler_for_X.transform(stats_vector)
 
     y_matrix = model.predict(X)
     
@@ -392,77 +265,19 @@ def view(history):
 
 # In[8]:
 
-# Model 1
-
-X, y, scaler_X, scaler_y = scale_data(*load_data())
-
-model = get_model()
-
-history = train_model(model, X, y, epochs=400, batch_size=100, validation_split=.10)
-
-model.save(os.path.join('models', 'overwatch-sr-1.h5'))
-
-view(history)
-
-
-# In[9]:
-
-# Model 2
-
-X, y, scaler_X2 = scale_data2(*load_data2())
-
-model2 = get_model2()
-
-history2 = train_model(model2, X, y, epochs=200, batch_size=100, validation_split=.10)
-
-model2.save(os.path.join('models', 'overwatch-sr-2.h5'))
-
-view(history2)
-
-
-# In[10]:
-
-# Model 3
-
-X, y, scaler_X3 = scale_data2(*load_data3())
-
-model3 = get_model3()
-
-history3 = train_model(model3, X, y, epochs=500, batch_size=100, validation_split=.10)
-
-model3.save(os.path.join('models', 'overwatch-sr-3.h5'))
-
-view(history3)
-
-
-# In[11]:
-
 # Model 4
 
-X, y, scaler_X4 = scale_data2(*load_data4())
+get_vector = lambda player, region : get_vector_herostats(player, region, stat_keys=general_hero_stats)
 
-model4 = get_model4()
+X, y, scaler_X = scale_data2(*load_data(get_vector))
 
-history4 = train_model(model4, X, y, epochs=500, batch_size=100, validation_split=.10)
+model = get_model4()
 
-model4.save(os.path.join('models', 'overwatch-sr-4.h5'))
+history = train_model(model, X, y, epochs=500, batch_size=100, validation_split=.10)
 
-view(history4)
+model.save(os.path.join('models', 'overwatch-sr-4.h5'))
 
-
-# In[12]:
-
-# Model 5
-
-X, y, scaler_X5 = scale_data2(*load_data5())
-
-model5 = get_model5()
-
-history5 = train_model(model5, X, y, epochs=500, batch_size=100, validation_split=.10)
-
-model5.save(os.path.join('models', 'overwatch-sr-5.h5'))
-
-view(history4)
+view(history)
 
 
 # In[ ]:
@@ -475,12 +290,7 @@ with open('test_names.txt', 'r') as test:
         player = Player.from_web_battletag(battletag)
         
         actual = get_competitive_rank(player, 'us')
-        p1 = predict_sr(model, player)
-        p2 = predict_sr2(model2, player)
-        p3 = predict_sr3(model3, player)
-        p4 = predict_sr4(model4, player)
-        p5 = predict_sr5(model5, player)
-        avg = (p1 + p2 + p3 + p4 + p5) / 5.
+        p = predict_sr2(model, player, scaler_X, get_vector)
         
-        print("{} is {}, predicted {}, {}, {}, {}, {}, avg {}".format(battletag, actual, p1, p2, p3, p4, p5, avg))
+        print("{} is {}, predicted {}".format(battletag, actual, p))
 
