d0c59bfdf0a2e9635d6fb124fd51410e46bb3e95
Shrivu Shankar <shrivu1122@gmail.com>
2017-12-20 16:16:43 -0600

    Updated App fixes #2

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
 
 from flask import Flask, render_template, jsonify
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+from keras.models import load_model
+import urllib.parse
+import requests
+import json
 import os
 
 app = Flask(__name__)
@@ -12,7 +17,15 @@ def index():
 @app.route('/predict/<battletag>')
 def predict(battletag=''):
 
-    response = jsonify({'battletag': battletag})
+    api_resp = requests.get('https://owapi.net/api/v3/u/{}/blob'.format(urllib.parse.quote_plus(battletag)), headers={'User-Agent':'OWAgent'}).text
+    api_json = json.loads(api_resp)
+
+    response = jsonify({
+        'battletag': battletag,
+        'api': api_json,
+        'actualrank': int(api_json['us']['stats']['competitive']['overall_stats']['comprank']),
+        'predictedrank': 0
+    })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
